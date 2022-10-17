6788a47031985f3c830891614a917480e6f0e40f
xaverp <xaverp@iiw.org>
2022-10-17 21:21:25 +0000

    Added submit method to clear input

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
     belongs_to :user
     validates :body, presence: true
+    scope :custom_display, -> { order(:created_at).last(20) } #GIVES YOU THE LAST 20 MESSAGES TO DISPLAY, TIED TO CHATROOM CONTROLLER AND CHATROOM INDEX
 end
